77841f942e28598eb4e88f9bc56178d6b1cd5207
titanium-cranium <titaniumcranium@icloud.com>
2015-12-29 02:00:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DPChess::Application.config.secret_key_base = '04ccd769cc03c2fde4466047d69dd299d6b968aa49ff5507293798d80df9b6d0a307a870ce3761888afbb907d4b342ce2ef0d4a56daf97dbabee7f2688d28249'
