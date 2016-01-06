271fb146af794c23e49094f0164fa304797466ff
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2016-01-06 01:38:17 +0000

    Added devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 DPChess::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Will be needed after heroku deployment
+  #config.action_mailer.default_url_options = { :host => 'deep-purple##.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
