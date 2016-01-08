b199838ca206781492553894f65f0c6d5919054b
Kyle Naizer <kyle.naizer@gmail.com>
2016-01-08 23:56:00 +0000

    Added populate board method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,57 @@
 class Game < ActiveRecord::Base
-end
+
+  has_many :players
+  has_many :pieces
+
+  after_create :populate_board!
+
+
+  def populate_board!
+
+    #White Pieces, this loop sets the entire row of pawns
+    (0..7).each do |i|
+      Pawn.create(
+        game_id: id,
+        x_position: i,
+        y_position: 6,
+        color: true  #assuming boolean value for white = true
+        )
+    end
+
+    Rook.create(game_id: id, x_position: 0, y_position: 0, color: true)
+    Rook.create(game_id: id, x_position: 7, y_position: 0, color: true)
+
+    Knight.create(game_id: id, x_position: 1, y_position: 0, color: true)
+    Knight.create(game_id: id, x_position: 6, y_position: 0, color: true)
+
+    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: true)
+    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: true)
+
+    Queen.create(game_id: id, x_position: 3, y_position: 0, color: true)
+    King.create(game_id: id, x_position: 4, y_position: 0, color: true )
+    end
+
+    #Black Pieces, this loop sets the entire row of pawns
+    (0..7).each do |i|
+      Pawn.create(
+        game_id: id,
+        x_position: i,
+        y_position: 1,
+        color: false  #assuming boolean value for white = true
+        )
+    end
+
+    Rook.create(game_id: id, x_position: 0, y_position: 7, color: false)
+    Rook.create(game_id: id, x_position: 7, y_position: 7, color: true)
+
+    Knight.create(game_id: id, x_position: 1, y_position: 7, color: false)
+    Knight.create(game_id: id, x_position: 6, y_position: 7, color: false)
+
+    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: false)
+    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: false)
+
+    Queen.create(game_id: id, x_position: 3, y_position: 7, color: false)
+    King.create(game_id: id, x_position: 4, y_position: 7, color: false)
+    end
+
+  end
