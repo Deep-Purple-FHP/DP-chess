8f96cc91a48bf555a2bfff9136eb7bc421cab81f
cameroniharris <cameroniharris1@gmail.com>
2016-01-11 22:27:11 +0000

    finished legal move methods

---
+++ test/models/piece_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+require 'model/pawn'
+
+class PieceTest < ActiveSupport::TestCase
+   test "Move A Pawn" do
+    pawn = Pawn.create(x_position:1,y_position:1)
+    assert !pawn.nil?
+   end
+  
+  
+end
