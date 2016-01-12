0a0eba57373154f5fe3708fd883b6413f146937a
titanium-cranium <titaniumcranium@icloud.com>
2016-01-12 23:38:28 +0000

    modified piece and game models to add icons, added new hash location lookup table

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,47 +13,49 @@ class Game < ActiveRecord::Base
 
     #White Pieces, this loop sets the entire row of pawns
     (0..7).each do |i|
-      Pawn.create(
+      Pawn.new(
         game_id: id,
         x_position: i,
         y_position: 1,
         color: 'white'
+        icon: 'white_pawn.png'
       )
     end
 
-    Rook.create(game_id: id, x_position: 0, y_position: 0, color: 'white')
-    Rook.create(game_id: id, x_position: 7, y_position: 0, color: 'white')
+    Rook.new(game_id: id, x_position: 0, y_position: 0, color: 'white', icon: 'white_rook.png')
+    Rook.new(game_id: id, x_position: 7, y_position: 0, color: 'white', icon: 'white_rook.png')
 
-    Knight.create(game_id: id, x_position: 1, y_position: 0, color: 'white')
-    Knight.create(game_id: id, x_position: 6, y_position: 0, color: 'white')
+    Knight.new(game_id: id, x_position: 1, y_position: 0, color: 'white', icon: 'white_knight.png')
+    Knight.new(game_id: id, x_position: 6, y_position: 0, color: 'white', icon: 'white_knight.png')
 
-    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: 'white')
-    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: 'white')
+    Bishop.new(game_id: id, x_position: 2, y_position: 0, color: 'white', icon: 'white_bishop.png')
+    Bishop.new(game_id: id, x_position: 5, y_position: 0, color: 'white', icon: 'white_bishop.png')
 
-    Queen.create(game_id: id, x_position: 3, y_position: 0, color: 'white')
-    King.create(game_id: id, x_position: 4, y_position: 0, color: 'white' )
+    Queen.new(game_id: id, x_position: 3, y_position: 0, color: 'white', icon: 'white_queen.png')
+    King.new(game_id: id, x_position: 4, y_position: 0, color: 'white', icon: 'white_king.png' )
 
     #Black Pieces, this loop sets the entire row of pawns
     (0..7).each do |i|
-      Pawn.create(
+      Pawn.new(
         game_id: id,
         x_position: i,
         y_position: 6,
         color: 'black'
+        icon: 'purple_pawn.png'
       )
     end
 
-    Rook.create(game_id: id, x_position: 0, y_position: 7, color: 'black')
-    Rook.create(game_id: id, x_position: 7, y_position: 7, color: 'black')
+    Rook.new(game_id: id, x_position: 0, y_position: 7, color: 'black', icon: 'purple_rook.png')
+    Rook.new(game_id: id, x_position: 7, y_position: 7, color: 'black', icon: 'purple_rook.png')
 
-    Knight.create(game_id: id, x_position: 1, y_position: 7, color: "black")
-    Knight.create(game_id: id, x_position: 6, y_position: 7, color: "black")
+    Knight.new(game_id: id, x_position: 1, y_position: 7, color: "black", icon: 'purple_knight.png')
+    Knight.new(game_id: id, x_position: 6, y_position: 7, color: "black", icon: 'purple_knight.png')
 
-    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: "black")
-    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: "black")
+    Bishop.new(game_id: id, x_position: 2, y_position: 7, color: "black", icon: 'purple_knight.png')
+    Bishop.new(game_id: id, x_position: 5, y_position: 7, color: "black", icon: 'purple_knight.png')
 
-    Queen.create(game_id: id, x_position: 3, y_position: 7, color: "black")
-    King.create(game_id: id, x_position: 4, y_position: 7, color: "black")
+    Queen.new(game_id: id, x_position: 3, y_position: 7, color: "black", icon: 'purple_queen.png')
+    King.new(game_id: id, x_position: 4, y_position: 7, color: "black", icon: 'purple_king.png')
   end
 
 end
