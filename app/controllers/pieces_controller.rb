f76e245476f242930974387500e3654c76a3c65c
titanium-cranium <titaniumcranium@icloud.com>
2016-01-13 22:26:24 +0000

    games_controller_spec now passes. Added (@game) as id

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,8 +1,15 @@
 class PiecesController < ApplicationController
 
   def update
-    piece_location = pieceLocation
-    render :json => {}, :status => 500
+    @origin_cell = pieceLocation
+    @destination_cell = pieceDestination
+    Piece.find_piece(origin_cell)
+
+
+
+
+
+
     # receives AJAX post request with piece_location and piece_destination
     # calls the piece move method to determine if valid
     # true = update view
@@ -10,7 +17,7 @@ class PiecesController < ApplicationController
 
   end
 
-
+  # From SO:  http://stackoverflow.com/questions/7237720/jquery-post-to-rails
   # def create
   #   @sub_comment = SubComment.new params['subcomment']
   #   if @sub_comment.save
--- spec/games_controller_spec.rb
+++ spec/games_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GamesController, type: :controller do
   describe "games#create action" do
     it "successfully creates a game" do
       post :create, :game => {:name => 'test', :state => "noidea", :white_player_id => 1, :black_player_id => 2, :winning_player_id => ""}
-      expect(response).to redirect_to game_path
+      expect(response).to redirect_to game_path(Game.last)
 
       game = Game.last
       expect(game.name).to eq("test")
