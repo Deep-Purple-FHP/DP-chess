d62d38d787aa99d571c55f6811c9da88af197a85
titanium-cranium <titaniumcranium@icloud.com>
2016-01-13 21:44:34 +0000

    rspec test still failing, now because I can't figure out how to pass the game id to the view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,7 +10,7 @@ class GamesController < ApplicationController
   end
 
   def show
-    @game = Game.find(1)
+    @game = Game.find(params[:id])
   end
 
 end
--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,10 +1,21 @@
 class PiecesController < ApplicationController
 
   def update
+    piece_location = pieceLocation
+    render :json => {}, :status => 500
     # receives AJAX post request with piece_location and piece_destination
     # calls the piece move method to determine if valid
     # true = update view
     # false = return alert (move invalid)
+
   end
 
-end
+
+  # def create
+  #   @sub_comment = SubComment.new params['subcomment']
+  #   if @sub_comment.save
+  #     render :json => { } # send back any data if necessary
+  #   else
+  #     render :json => { }, :status => 500
+  #   end
+  # end
